26c6899d1fd2260e28136ee832bc481519824113
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-05-10 14:20:56 -0700

    Adds cable control, and async read/write infrared functions.

--- main.ts
+++ main.ts
@@ -119,6 +119,59 @@ let appletv = {
     tv: [0],
 }
 
+let cable = {
+    power: [0],
+    up: [0],
+    down: [0],
+    left: [0],
+    right: [0],
+    ok: [0],
+    back: [0],
+    chup: [0],
+    chdown: [0],
+    record: [0],
+    rewind: [0],
+    fastforward: [0],
+    playpause: [0],
+    ondemand: [0],
+    guide: [0],
+    info: [0],
+    exit: [0],
+    a: [0],
+    b: [0],
+    c: [0],
+    d: [0],
+    num1: [0],
+    num2: [0],
+    num3: [0],
+    num4: [0],
+    num5: [0],
+    num6: [0],
+    num7: [0],
+    num8: [0],
+    num9: [0],
+    num0: [0],
+}
+
+let projector = {
+    power: [0],
+    up: [0],
+    down: [0],
+    left: [0],
+    right: [0],
+    ok: [0],
+    back: [0],
+    menu: [0],
+    freeze: [0],
+    zoomin: [0],
+    zoomout: [0],
+    source: [0],
+    eco: [0],
+    volup: [0],
+    voldown: [0],
+    mute: [0],
+}
+
 
 
 
@@ -150,5 +203,45 @@ let infared_input = {
         } else {
             return abc;
         }
+    }
+}
+
+let infared_output = {
+    write: function (abc: number) {
+        pins.digitalWritePin(DigitalPin.P0, abc);
+    }
+}
+
+
+let device = television;
+let button = device.power;
+let i = 0;
+
+let reading = false;
+let writing = false;
+
+async function writeto (obj:Object, but:string) {
+    let abc: number[] = (<any>obj)[but];
+    reading = true;
+    (<any>obj)[but] = [];
+    let started = false;
+    while (reading) {
+        if(!started) {
+            if(infared_input.read() == 1) {
+                started = true;
+            }
+        } else {
+            (<any>obj)[but].push(infared_input.read());
+        }
+    }
+}
+
+async function readfrom (obj:Object, but:string) {
+    let abc: number[] = (<any>obj)[but];
+    writing = true;
+    i = 0;
+    while (writing) {
+        infared_output.write(abc[i]);
+        i++;
     }
 }
